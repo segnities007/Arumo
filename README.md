0ab9d5a6a7c08417d5fd1719b7fc3af6a39830d6
seg <segnities007@gmail.com>
2025-07-14 12:13:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# レシートを用いた在庫管理による多機能アプリ「Arumo」
+
+## なにこれ
+
+- レシート×消耗品管理アプリ
+
+### メンバー
+
+- Segnities007: フルスタック
+
+### 技術スタック
+
+- クライアント
+    - 言語
+        - Kotlin
+    - フレームワーク等
+        - Jetpack Compose
+        - Compose Multiplatform
+    - ライブラリ等
+        - Room
+        - Koin
+    - アーキテクチャ
+        - MVI
+        - Clean Architecture
+- サーバーサイド
+    - Supabase
---
+++ composeApp/src/commonMain/kotlin/com/segnities007/arumon/App.kt
@@ -0,0 +1,26 @@
+package com.segnities007.arumon
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import arumon.composeapp.generated.resources.Res
+import arumon.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+    }
+}
